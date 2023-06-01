18fc204dc6b5182f198b11ed007dd64c772b1ffb
Romast27 <108524027+Romast27@users.noreply.github.com>
2023-06-01 13:58:29 +0300

    Delete Project_school_site directory

--- Project_school_site/forms/comment.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class CommentForm(FlaskForm):
-    content = TextAreaField("Коментарий", validators=[DataRequired()])
-    submit = SubmitField('Применить')
